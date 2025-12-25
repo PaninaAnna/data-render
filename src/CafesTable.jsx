91254afb0ed04d543cbd9a875f0abacdc1c6f130
anya96 <papanina96@yandex.ru>
2025-12-26 01:39:00 +0300

    add step 1

---
+++ src/CafesTable.jsx
@@ -0,0 +1,11 @@
+import FilterCafes from './FilterCafes';
+
+const CafesTable = () => {
+  return (
+    <div className="cafesTable">
+      <FilterCafes />
+    </div>
+  );
+};
+
+export default CafesTable;
